6de621c5977d2dee79aed03a1e8fdca9195401b5
Zach_Jordan_ <zach.jordan505@gmail.com>
2024-04-21 11:12:29 -0600

    re validated products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,16 @@ class Product < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def category_names
     categories.pluck(:name).join(', ')
   end
 
+  private
+
+  def image_attached
+    errors.add(:image, "must be attached") unless image.attached?
+  end
 end
